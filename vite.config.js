bdf7b3bd1a47db84acfe66515ff0b8a8238e6eeb
sahidbalwa <138686668+sahidbalwa@users.noreply.github.com>
2025-01-27 17:52:44 +0530

    solve the error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Expose the server to the network
+    port: process.env.PORT || 5173, // Use Render's PORT variable or default to 5173
+  },
 })
